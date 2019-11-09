b7e1f6d200a19c1192842fcac6d340e2a635ce02
Zli <3477107094@qq.com>
2019-11-09 22:33:34 +0800

    认证后的消息提示

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
+use Illuminate\Foundation\Auth\VerifiesEmails;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
 
@@ -17,6 +18,9 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
         Registered::class => [
             SendEmailVerificationNotification::class,
+        ],
+        \Illuminate\Auth\Events\Verified::class => [
+            \App\Listeners\EmailVerified::class,
         ],
     ];
 
--- resources/shared/_messages.blade.php
+++
@@ -1,9 +0,0 @@
-@foreach (['info', 'warning', 'success', 'danger'] as $msg)
-  @if (session()->has($msg))
-    <div class="flash-message">
-      <p class="alert alert-{{$msg}}">
-        {{session()->get($msg)}}
-      </p>
-    </div>
-  @endif
-@endforeach
