aa60bf65eaac3164553c265c93e705451ae5a024
Zli <3477107094@qq.com>
2019-11-10 14:43:00 +0800

    上传头像

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Handlers\ImageUploadHandler;
 use App\Http\Requests\UserRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -9,17 +10,28 @@ use Illuminate\Http\Request;
 class UsersController extends Controller
 {
     //
-    public function show(User $user){
+    public function show(User $user)
+    {
         return view('users.show', compact('user'));
     }
 
-    public function edit(User $user){
-        return view('users.edit',compact('user'));
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
     }
 
-    public function update(UserRequest $request, User $user)
+    public function update(UserRequest $request, ImageUploadHandler $uploader, User $user)
     {
-        $user->update($request->all());
+        $data = $request->all();
+
+        if ($request->avatar) {
+            $result = $uploader->save($request->avatar, 'avatars', $user->id);
+            if ($result) {
+                $data['avatar'] = $result['path'];
+            }
+        }
+
+        $user->update($data);
         return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
     }
 }
