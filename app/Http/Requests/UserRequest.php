549c3a4e2ac7e4c802416470bf6de7f7ccfd6f28
Zli <3477107094@qq.com>
2019-11-10 14:51:29 +0800

    显示头像

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -25,7 +25,7 @@ class UserRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => 'required|between:3,10|unique:users,name,' . Auth::id(),
+            'name' => 'required|between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name,' . Auth::id(),
             'email' => 'required|email',
             'introduction' => 'max:80',
         ];
